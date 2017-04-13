818ce4063215c07f5dec768725c0a1c375fcc9ec
lauli <laureen.schausberger@students.fh-hagenberg.at>
2017-04-13 20:35:47 +0200

    Add files via upload
    got speak tags working ! :)

--- lambda_handler.py
+++ lambda_handler.py
@@ -15,16 +15,16 @@ import quotes_scrape
 import boto3
 
 
-def build_speechlet_response(title, output, reprompt_text, should_end_session):
+def build_speechlet_response(title, output, content, reprompt_text, should_end_session):
     return {
         'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
+            'type': 'SSML',
+            'ssml': output
         },
         'card': {
             'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
+            'title': "My Best Friend - " + title,
+            'content': content
         },
         'reprompt': {
             'outputSpeech': {
@@ -49,11 +49,11 @@ def build_response(session_attributes, speechlet_response):
 # --------------- Events ------------------
 
 def on_session_started(session_started_request, session):
-    return build_response({"topic": "none", "index": -1, "last_quote": "none"}, build_speechlet_response("", "", "", False))
+    return build_response({"topic": "none", "index": -1, "last_quote": "none"}, build_speechlet_response("Start", "<speak></speak>", "", "", False))
 
 
 def on_launch(launch_request, session):
-    return build_response({"topic": "none", "index": -1, "last_quote": "none"}, build_speechlet_response("Welcome.", "Welcome. How can I help you?", "If you want to be motivated, just ask me to tell you a quote.", False))
+    return build_response({"topic": "none", "index": -1, "last_quote": "none"}, build_speechlet_response("Welcome.", "<speak>Welcome. How can I help you?</speak>", "", "If you want to be motivated, just ask me to tell you a quote.", False))
 
 def on_session_ended(session_ended_request, session):
     """ Called when the user ends the session.
@@ -67,10 +67,10 @@ def on_session_ended(session_ended_request, session):
 
 def handle_session_end_request():
     card_title = "Skill Ended"
-    speech_output = "Okay. Bye"
+    speech_output = "<speak>Okay. Bye</speak>"
     should_end_session = True
     return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
+        card_title, speech_output, "", None, should_end_session))
 
 
 # --------------- Main handler ------------------
@@ -121,19 +121,20 @@ def lambda_handler(event, context):
 # --------------- Intents ------------------
 
 def help():
-    return build_response({}, build_speechlet_response("Help","You can ask me to inspire you about specific topics", "You could for example say: motivate me about work.", False))
+    return build_response({}, build_speechlet_response("Help","<speak>You can ask me to inspire you about specific topics</speak>", "help", "You could for example say: motivate me about work.", False))
 
 def list_quotes(topic, session):
     quotes = quotes_scrape.get_quotes(topic)
     context = '<speak> '
-
+    app_content = ''
 
     for quote in quotes:
-        context = context + quote + ' <break time=\"2s\" />'
+        context = context + quote + ' <break time=\"3s\"/> '
+        app_content = app_content + "\n" + quote
 
     context = context + ' </speak>'
 
-    return build_response({"topic": topic, "index": -1, "last_quote": "none"}, build_speechlet_response("Quotes", context, "", True))
+    return build_response({"topic": topic, "index": -1, "last_quote": "none"}, build_speechlet_response("Quotes", context, app_content, "", True))
 
 def tell_one_quote(topic, session):
     quotes = quotes_scrape.get_quotes(topic)
@@ -145,7 +146,7 @@ def tell_one_quote(topic, session):
 
     print(index)
     context = '<speak> ' + quotes[index] + ' </speak>'
-    return build_response({"topic": topic, "index": index, "last_quote": quotes[index]}, build_speechlet_response("Quote", context, "Would you like a new one? Or do you want to do something with this quote?", False))
+    return build_response({"topic": topic, "index": index, "last_quote": quotes[index]}, build_speechlet_response("Quote", context, quotes[index], "Would you like a new one? Or do you want to do something with this quote?", False))
 
 
 def todos_sns_topic():
@@ -156,4 +157,4 @@ def send_to_email(session):
     message = session['attributes']['last_quote']
     todos_sns_topic().publish(Message=message, Subject="Here is the awesome quote")
 
-    return build_response({"topic": session['attributes']['topic'], "index": session['attributes']['index'], "last_quote": session['attributes']['last_quote']}, build_speechlet_response("Success", "I successfully shared the quote.", "I'm still here", False))
+    return build_response({"topic": session['attributes']['topic'], "index": session['attributes']['index'], "last_quote": session['attributes']['last_quote']}, build_speechlet_response("Success", "<speak>I successfully shared the quote.</speak>", "I successfully shared: " + session['attributes']['last_quote'], "I'm still here", False))
