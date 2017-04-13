a723f7770f8aefb28fa2443a766c48ceaae7fda9
lambrina <lambrina.kostova@gmail.com>
2017-04-13 20:57:06 +0100

    add functionality for author extension in scraping

--- quotes_scrape.py
+++ quotes_scrape.py
@@ -2,20 +2,37 @@
 from lxml import html
 import requests
 
-
+quotes_authors = {}
 def get_quotes(keyWord):
     quotes = []
+    authors = []
     for i in range(1, 5):
         url = "https://www.quotesdaddy.com/find/tag/" + keyWord + "/" + str(i)
         str_data = requests.get(url)
         tree = html.fromstring(str_data.content)
         page_quotes = tree.xpath('//div[@class = "quoteText"]/p/a/text()')
+        page_authors = tree.xpath('//div[@class = "quoteAuthorName"]/p/a/text()')
         num_quotes = len(page_quotes)
         quotes+=page_quotes
+        authors+=page_authors
     quotes = list(map(lambda q: q[1:-1], quotes))
+    global quotes_authors
+    quotes_authors = dict(zip(quotes, authors))
     return quotes
 
+def get_author(quote):
+    author  = quotes_authors.get(quote)
+    return author
 
 
-for q in get_quotes("sports"):
-    print q
+# for testing
+d = get_quotes("sports")
+print quotes_authors
+
+random_quote = d[1]
+print random_quote
+print get_author(random_quote)
+
+#for q in get_quotes("sports"):
+#    print q
+#    print get_author()
