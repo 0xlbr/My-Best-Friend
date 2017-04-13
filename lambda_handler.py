ad6137dec74c06163e06c4c274372bdce9e42279
lauli <laureen.schausberger@students.fh-hagenberg.at>
2017-04-13 19:49:34 +0200

    Add files via upload
    added sharing option

--- lambda_handler.py
+++ lambda_handler.py
@@ -12,6 +12,7 @@ from __future__ import print_function
 
 # --------------- Helpers that build all of the responses ----------------------
 import quotes_scrape
+import boto3
 
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
@@ -48,11 +49,11 @@ def build_response(session_attributes, speechlet_response):
 # --------------- Events ------------------
 
 def on_session_started(session_started_request, session):
-    """ Called when the session starts """
+    return build_response({"topic": "none", "index": -1, "last_quote": "none"}, build_speechlet_response("", "", "", False))
 
 
 def on_launch(launch_request, session):
-    return build_response({'topic': 'none', 'index': -1}, build_speechlet_response("Welcome.", "Welcome. How can I help you?", "If you want to be motivated, just ask me to tell you a quote.", False))
+    return build_response({"topic": "none", "index": -1, "last_quote": "none"}, build_speechlet_response("Welcome.", "Welcome. How can I help you?", "If you want to be motivated, just ask me to tell you a quote.", False))
 
 def on_session_ended(session_ended_request, session):
     """ Called when the user ends the session.
@@ -110,33 +111,49 @@ def lambda_handler(event, context):
         elif event['request']['intent']['name'] == "QuoteAnotherIntent":
             return tell_one_quote(event['session']['attributes']['topic'] , event['session'])
 
+        elif event['request']['intent']['name'] == "ShareQuoteIntent":
+            return send_to_email(event['session'])
+
     elif event['request']['type'] == "SessionEndedRequest":
         return on_session_ended(event['request'], event['session'])
 
+
+# --------------- Intents ------------------
 
 def help():
     return build_response({}, build_speechlet_response("Help","You can ask me to inspire you about specific topics", "You could for example say: motivate me about work.", False))
 
 def list_quotes(topic, session):
     quotes = quotes_scrape.get_quotes(topic)
-    context = "<speak>"
+    context = '<speak> '
+
 
     for quote in quotes:
-        context = context + quote + "<break time=\"2s\" />"
+        context = context + quote + ' <break time=\"2s\" />'
 
-    context = context + "</speak>"
-    return build_speechlet_response("Quotes", context, "", True)
+    context = context + ' </speak>'
+
+    return build_response({"topic": topic, "index": -1, "last_quote": "none"}, build_speechlet_response("Quotes", context, "", True))
 
 def tell_one_quote(topic, session):
     quotes = quotes_scrape.get_quotes(topic)
-    index = session['attributes']['index'] + 1
-    context = "<speak>" +  quotes[index] + "</speak>"
-    session.attributes['last_quote'] = quotes[0]
+    print(session['attributes'])
+    if (session['attributes'] == {}):
+        index = 0
+    else:
+        index = session['attributes']['index'] + 1
 
-    return build_speechlet_response({'topic'}, "Quote", quotes[0], "Would you like a new one? Or do you want to do something with this quote?", False)
+    print(index)
+    context = '<speak> ' + quotes[index] + ' </speak>'
+    return build_response({"topic": topic, "index": index, "last_quote": quotes[index]}, build_speechlet_response("Quote", context, "Would you like a new one? Or do you want to do something with this quote?", False))
 
-def share_last_quote(time):
-   # if session.attributes['current_quote'] != "":
-    #    return statement("Sorry, I can't find the last quote.")
-        #sharing string that's currently saved in session.attributes['current_quote']
-    return build_speechlet_response("I successfully shared the quote.")
+
+def todos_sns_topic():
+    return boto3.resource('sns').Topic('arn:aws:sns:eu-west-1:847828999320:sharequote')
+
+
+def send_to_email(session):
+    message = session['attributes']['last_quote']
+    todos_sns_topic().publish(Message=message, Subject="Here is the awesome quote")
+
+    return build_response({"topic": session['attributes']['topic'], "index": session['attributes']['index'], "last_quote": session['attributes']['last_quote']}, build_speechlet_response("Success", "I successfully shared the quote.", "I'm still here", False))
